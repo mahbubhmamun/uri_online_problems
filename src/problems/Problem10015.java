f75bf17655b114c696707434c6c2abe443a33a7d
mahbubhmamun <hussainmahbub88@gmail.com>
2018-12-06 18:02:18 -0500

    Problem 10015 done

---
+++ src/problems/Problem10015.java
@@ -0,0 +1,16 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Problem10015 {
+	static Scanner sc = new Scanner (System.in);
+	public static void main(String[] args) {
+		double x1 = sc.nextDouble();
+		double y1 = sc.nextDouble();
+		double x2 = sc.nextDouble();
+		double y2 = sc.nextDouble();
+		
+		double distance = Math.sqrt((Math.pow((x2 - x1),2)) + (Math.pow((y2-y1), 2)));
+		System.out.printf("%.4f \n",distance);
+	}
+}
