2d6b94ed6f09a16dc8fb9cbf6303c828e2f89a6e
mahbubhmamun <hussainmahbub88@gmail.com>
2018-12-06 17:43:56 -0500

    Problem 10014 done

---
+++ src/problems/Problem10014.java
@@ -0,0 +1,17 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Problem10014 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		double Y = sc.nextDouble();
+
+		double average = X / Y;
+		System.out.printf("%.3f km/l\n", average);
+
+	}
+
+}
